7bc998f64196fbe480be33e32960bff0ae5450ac
Eric Kim <deveric89@naver.com>
2024-11-27 22:04:53 +0900

    UrbanShop
    DB Connection Tested
    Account Table Created
    Navbar and Logo Created

---
+++ UrbanShop/UrbanShop/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using UrbanShop.Models;
+
+namespace UrbanShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Account> Account { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ UrbanShop/UrbanShop/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrbanShop.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Account_ID { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string User_Account { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string User_Password { get; set; }    
+    }
+}
